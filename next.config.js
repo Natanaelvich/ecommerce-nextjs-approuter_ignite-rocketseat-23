e0e29db7bdc0439122e173694665ba2113a10145
natanaelvich <taelima1997@gmail.com>
2023-10-29 22:56:51 -0300

    fix lint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['github.com'],
-    },
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 module.exports = nextConfig
